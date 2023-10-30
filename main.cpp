02673e517c580bb88452c368919111e8963034a7
Tastypotato245 <rbt2775@cau.ac.kr>
2023-10-30 21:33:18 +0900

    test: test code added

--- main.cpp
+++ main.cpp
@@ -6,16 +6,16 @@
 int main(int argc, char **argv)
 {
     std::string input;
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
-        return 1;
-    }
-
-    std::ifstream file(argv[1]);
-    if (!file.is_open()) {
-        std::cerr << "Failed to open file: " << argv[1] << std::endl;
-        return 1;
-    }
+//    if (argc != 2) {
+//        std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
+//        return 1;
+//    }
+//
+//    std::ifstream file(argv[1]);
+//    if (!file.is_open()) {
+//        std::cerr << "Failed to open file: " << argv[1] << std::endl;
+//        return 1;
+//    }
 
 //    std::getline(file, input, '\0');
 //    file.close();
